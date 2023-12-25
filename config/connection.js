413b7a4b0c5b164af6233169b89254d0ff0ee92a
mariamdawood <mariam.miladd@gmail.com>
2023-12-25 12:19:53 -0600

    debugging

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,17 @@
-const Sequelize = require('sequelize')
-require('dotenv').config()
+const Sequelize = require('sequelize');
+require('dotenv').config();
 
 const sequelize = process.env.JAWSDB_URL
-    ? new Sequelize(process.env.JAWSDB_URL)
+    ? new Sequelize(process.env.JAWSDB_URL, {
+        logging: console.log,
+    })
     : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
         host: 'localhost',
         dialect: 'mysql',
         dialectOptions: {
             decimalNumbers: true,
         },
-    })
+        logging: console.log,
+    });
 
-module.exports = sequelize
+module.exports = sequelize;
