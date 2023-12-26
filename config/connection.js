f9161dded259164e1d4f8b55dff183dd638a7d30
mariamdawood <mariam.miladd@gmail.com>
2023-12-26 11:56:41 -0600

    debugging

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,11 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-    // Production environment on Heroku
-    sequelize = new Sequelize(process.env.JAWSDB_URL, {
+const sequelize = process.env.JAWSDB_URL
+    ? new Sequelize(process.env.JAWSDB_URL, {
         logging: console.log,
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false,
-            },
-        },
-    });
-} else {
-    // Local development environment
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    })
+    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
         host: 'localhost',
         dialect: 'mysql',
         dialectOptions: {
@@ -24,7 +13,6 @@ if (process.env.JAWSDB_URL) {
         },
         logging: console.log,
     });
-}
 
 sequelize
     .authenticate()
