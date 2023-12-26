577adc38960ff35ea16dc110dd66e7c01f5984d9
mariamdawood <mariam.miladd@gmail.com>
2023-12-26 13:39:32 -0600

    debugging

--- config/connection.js
+++ config/connection.js
@@ -26,16 +26,16 @@
 // module.exports = sequelize;
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'mysql',
-    dialectOptions: {
-        decimalNumbers: true,
-    },
-});
-// : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-//     host: process.env.DB_HOST || 'localhost',
-//     dialect: 'mysql',
-// });
+const sequelize = process.env.DATABASE_URL
+    ? new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'mysql',
+        dialectOptions: {
+            decimalNumbers: true,
+        },
+    })
+    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+        dialect: 'mysql',
+    });
 
 sequelize
     .authenticate()
