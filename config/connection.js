225c6a97599107205ff6b64a90836b544d4e3479
mariamdawood <mariam.miladd@gmail.com>
2023-12-26 15:42:04 -0600

    deployment

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ const sequelize = process.env.JAWSDB_URL
         logging: console.log,
     })
     : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-        // host: 'localhost',
+        host: 'localhost',
         dialect: 'mysql',
         dialectOptions: {
             decimalNumbers: true,
