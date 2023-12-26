a3f19d62ec246cc5e6583f33497641ad633f0116
mariamdawood <mariam.miladd@gmail.com>
2023-12-26 13:20:13 -0600

    deployment

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,38 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+// const Sequelize = require('sequelize');
+// require('dotenv').config();
 
-const sequelize = process.env.JAWSDB_URL
-    ? new Sequelize(process.env.JAWSDB_URL, {
-        logging: console.log,
+// const sequelize = process.env.JAWSDB_URL
+//     ? new Sequelize(process.env.JAWSDB_URL, {
+//         logging: console.log,
+//     })
+//     : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//         host: 'localhost',
+//         dialect: 'mysql',
+//         dialectOptions: {
+//             decimalNumbers: true,
+//         },
+//         logging: console.log,
+//     });
+
+// sequelize
+//     .authenticate()
+//     .then(() => {
+//         console.log('Connection has been established successfully.');
+//     })
+//     .catch((err) => {
+//         console.error('Unable to connect to the database:', err);
+//     });
+
+// module.exports = sequelize;
+const { Sequelize } = require('sequelize');
+
+const sequelize = process.env.DATABASE_URL
+    ? new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'mysql',
     })
     : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
         host: 'localhost',
         dialect: 'mysql',
-        dialectOptions: {
-            decimalNumbers: true,
-        },
-        logging: console.log,
     });
 
 sequelize
