ad4c07b4fd73aa81bcb4b0495496043cb0b27b60
mariamdawood <mariam.miladd@gmail.com>
2023-12-26 13:29:36 -0600

    debugging

--- config/connection.js
+++ config/connection.js
@@ -29,9 +29,12 @@ const { Sequelize } = require('sequelize');
 const sequelize = process.env.DATABASE_URL
     ? new Sequelize(process.env.DATABASE_URL, {
         dialect: 'mysql',
+        dialectOptions: {
+            decimalNumbers: true,
+        },
     })
     : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-        host: 'localhost',
+        host: process.env.DB_HOST || 'localhost',
         dialect: 'mysql',
     });
 
